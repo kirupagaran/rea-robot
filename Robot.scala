9d69f9b4015ff9b3f1a840738db70c4cc4fb339b
kirupagaran <kirupagaran.d@gmail.com>
2016-04-17 15:37:56 +1000

    v1.1 - Added logic for MOVE, RIGHT and LEFT commands. Deleted class files and readme.txt

--- Robot.scala
+++ Robot.scala
@@ -1,32 +1,79 @@
-object Robot {
+object Robot{
   def main(args:Array[String]){
     val lines = io.Source.fromFile("data/data.txt").getLines();
-    val tup = (1,1,"N")
+    
     val flag = (0,1,"flag");
     val commands = lines.toTraversable.toArray
+    
     //println(commands(1))
     //for(line<-lines){
-      println(calculate(commands,tup,flag))  
+    if(commands(0).split(" ")(0).toLowerCase() == "place"){
+      val coordinates = commands(0).split(" ")(1).split(",")
+      val tup = (coordinates(0).toInt,coordinates(1).toInt,coordinates(2))
+      //println(tup)
+      println(calculate(commands,tup,flag))
+    }
+    else
+       println("Not a valid command. Please place the robot on table first");
+        
     //}
     
   }
   def calculate(commands:Array[String],tup:(Int,Int,String),flag:(Int,Int,String)):String={
-    println(commands(flag._1))
-    commands(flag._1) match {
-      case "place"  => place(commands,tup,flag)
+    //println(commands(flag._1))
+    val a =1;
+    commands(flag._1).toLowerCase().split(" ")(0) match {
+      case "place" => place(commands,(commands(flag._1).split(" ")(1).split(",")(0).toInt,commands(flag._1).split(" ")(1).split(",")(1).toInt,commands(flag._1).split(" ")(1).split(",")(2)),flag)
       case "move"  => move(commands,tup,flag)
-      //case "left"  => return place(commands,tup,flag)
-      //case "right"  => return place(commands,tup,flag)
+      case "right"  => return right(commands,tup,flag)
+      case "left"  => return left(commands,tup,flag)
       case "report"  => return report(commands,tup,flag)
     }
   }
   def place(commands:Array[String],tup:(Int,Int,String),flag:(Int,Int,String))={
-    calculate(commands:Array[String],(tup._1,tup._2,"N"),(flag._1+1,flag._2,flag._3))
+    calculate(commands:Array[String],(tup._1,tup._2,tup._3),(flag._1+1,flag._2,flag._3))
   }
   def move(commands:Array[String],tup:(Int,Int,String),flag:(Int,Int,String))={
-    calculate(commands:Array[String],(tup._1+1,tup._2+2,"N"),(flag._1+1,flag._2,flag._3))
+    if(tup._3.toLowerCase() == "north"){
+     calculate(commands:Array[String],(tup._1,tup._2+1,tup._3),(flag._1+1,flag._2,flag._3))
+    }
+    else if(tup._3.toLowerCase() == "south"){
+      calculate(commands:Array[String],(tup._1,tup._2-1,tup._3),(flag._1+1,flag._2,flag._3))
+    }
+    else if(tup._3.toLowerCase() == "east"){
+      calculate(commands:Array[String],(tup._1+1,tup._2,tup._3),(flag._1+1,flag._2,flag._3))
+    }
+    else{
+      calculate(commands:Array[String],(tup._1-1,tup._2,tup._3),(flag._1+1,flag._2,flag._3))
+    }
   }
-  def report(commands:Array[String],tup:(Int,Int,String),flag:(Int,Int,String)):String={
-    return tup._1+","+tup._2+","+tup._3;
+  def right(commands:Array[String],tup:(Int,Int,String),flag:(Int,Int,String))={
+    if(tup._3.toLowerCase() == "north"){
+     calculate(commands:Array[String],(tup._1,tup._2,"east"),(flag._1+1,flag._2,flag._3))
+    }
+    else if(tup._3.toLowerCase() == "south"){
+      calculate(commands:Array[String],(tup._1,tup._2,"west"),(flag._1+1,flag._2,flag._3))
+    }
+    else if(tup._3.toLowerCase() == "east"){
+      calculate(commands:Array[String],(tup._1,tup._2,"south"),(flag._1+1,flag._2,flag._3))
+    }
+    else{
+      calculate(commands:Array[String],(tup._1,tup._2,"north"),(flag._1+1,flag._2,flag._3))
+    }
   }
-}
+  
+  def left(commands:Array[String],tup:(Int,Int,String),flag:(Int,Int,String))={
+    if(tup._3.toLowerCase() == "north"){
+     calculate(commands:Array[String],(tup._1,tup._2,"west"),(flag._1+1,flag._2,flag._3))
+    }
+    else if(tup._3.toLowerCase() == "south"){
+      calculate(commands:Array[String],(tup._1,tup._2,"east"),(flag._1+1,flag._2,flag._3))
+    }
+    else if(tup._3.toLowerCase() == "east"){
+      calculate(commands:Array[String],(tup._1,tup._2,"north"),(flag._1+1,flag._2,flag._3))
+    }
+    else{
+      calculate(commands:Array[String],(tup._1,tup._2,"south"),(flag._1+1,flag._2,flag._3))
+	}
+  }
+ }
